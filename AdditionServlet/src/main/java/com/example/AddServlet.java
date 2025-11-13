3997f7ca83ff07f8e7dc125b3716b33028a9245c
sayaligurav7789 <sayligurab7789@gmail.com>
2025-11-13 20:06:35 +0530

    AddServlet using res.getWriter through PrintWriter and printing on server

--- AdditionServlet/src/main/java/com/example/AddServlet.java
+++ AdditionServlet/src/main/java/com/example/AddServlet.java
@@ -1,17 +1,21 @@
 package com.example;
 
+import java.io.IOException;
+import java.io.PrintWriter;
+
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 public class AddServlet extends HttpServlet{
 
-	public void service(HttpServletRequest req, HttpServletResponse res) {
+	public void service(HttpServletRequest req, HttpServletResponse res) throws IOException {
 		int i = Integer.parseInt(req.getParameter("num1"));
 		int j = Integer.parseInt(req.getParameter("num2"));
 		
 		int k = i + j;
 		
-		System.out.println("Result is: "+k);
+		PrintWriter out = res.getWriter();
+		out.println("Result is: "+k);
 	}
 }
