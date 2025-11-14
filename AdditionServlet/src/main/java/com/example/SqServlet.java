5c75a7a1cd8a5a3f864819ccbd55432b50617720
sayaligurav7789 <sayligurab7789@gmail.com>
2025-11-14 13:10:09 +0530

    Using RequestDispatcher.forward() for calling one servlet from another

---
+++ AdditionServlet/src/main/java/com/example/SqServlet.java
@@ -0,0 +1,16 @@
+package com.example;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class SqServlet extends HttpServlet{
+	public void doPost(HttpServletRequest req, HttpServletResponse res) throws IOException {
+		PrintWriter out = res.getWriter();
+		out.println("Hello to Square Servlet!");
+		
+	}
+}
